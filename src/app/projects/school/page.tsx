957475ba62b0df566a42c767c43a268187d34086
PhillipLDaae1 <e-phidaa@iskule.no>
2024-01-31 17:04:24 +0100

    Fixed alignment in school projects.

--- src/app/projects/school/page.tsx
+++ src/app/projects/school/page.tsx
@@ -14,7 +14,7 @@ export default function School(){
                             alt="Picture of my school webpage"
                         />
                         <div className="flex flex-col items-center p-5">
-                            <h2 className="text-2xl m-4">School webpage</h2>
+                            <h2 className="text-2xl m-4 text-center">School webpage</h2>
                             <p className="text-center">This is the webpage I made for my school, feel free to take a look!</p>
                             <a className="px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600 transition-all m-4" href="https://phillipldaae1.github.io/Amalie_Skram_IM_Linjen/index.html">Link</a>
                         </div>
